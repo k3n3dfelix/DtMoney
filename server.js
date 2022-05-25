24f7bbfb78aba30fae9764e0c7f6ec8ca96d6a78
Kened Silva Felix <ksfelix1@stefanini.com>
2022-05-25 15:36:55 -0300

    Configuração express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const { resolve } = require ('path');
+
+const app = express();
+
+app.use('/', 
+        express.static(
+            resolve(
+                __dirname,
+                './build'
+            ))
+)
+
+app.listen(proces.env.PORT || 300, (err)=> {
+    if(err) { return console.log(err); }
+})
